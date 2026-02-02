6e72ae540ed3676e622f9b453e1ff38191b39efe
shriraj kharade <shrirajkharade@gmail.com>
2026-02-02 22:03:56 +0530

    variables

---
+++ variables.java
@@ -0,0 +1,14 @@
+public class variables {
+    public static void main(String[] args) {
+        int a=8;
+        int b=4;
+        int c=7;
+        
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        
+    }
+}
