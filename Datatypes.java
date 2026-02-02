4b6d6997502ec1b5570d562af99622d51634a0f2
shriraj kharade <shrirajkharade@gmail.com>
2026-02-02 22:27:33 +0530

    data types

---
+++ Datatypes.java
@@ -0,0 +1,15 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+        char ch ='s';
+        System.out.println(ch);
+        int d = 15;
+        System.out.println(d);
+        float p = 17.2f;
+        System.out.println(p);
+        double a =123.456;
+        System.out.println(a);
+
+    }
+}
