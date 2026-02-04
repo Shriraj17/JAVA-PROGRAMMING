1a8132e5eb80baf42104364bbfbbd8b68ffcd352
shriraj kharade <shrirajkharade@gmail.com>
2026-02-04 22:09:25 +0530

    casting 2

---
+++ casting2.java
@@ -0,0 +1,13 @@
+
+
+public class casting2 {
+    public static void main(String[] args) {
+        char ch ='a';
+        char ch2 ='b';
+        int number = (int) ch;
+        int number2 = (int) ch2;
+        System.out.println(number);
+        System.out.println(number2);
+
+    }
+}
