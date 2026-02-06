eb9e9902dd32126c6533df791b1ea840d68cc793
shriraj kharade <shrirajkharade@gmail.com>
2026-02-06 22:05:17 +0530

    type promotion

---
+++ promotion2.java
@@ -0,0 +1,10 @@
+public class promotion2 {
+    public static void main(String[] args) {
+        int a = 17;
+        float b = 22.17f;
+        long c = 26;
+        double d = 40.15;
+        double ans = a+b+c+d;
+        System.out.println(ans);
+    }
+}
