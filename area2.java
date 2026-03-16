d4ee5079d0dea522de256535bc9ed53cbd45f701
shriraj kharade <shrirajkharade@gmail.com>
2026-03-16 16:08:28 +0530

    area of squre

---
+++ area2.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class area2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int side = sc.nextInt();
+
+        int area = side*side;
+
+        System.out.println("area of the squre is : " + area);
+    }
+
+    
+}
