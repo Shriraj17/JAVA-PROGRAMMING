036d9eae34eca52422bd814aebd44e87c3ccc057
shriraj kharade <shrirajkharade@gmail.com>
2026-05-04 11:02:43 +0530

    unary operators

---
+++ unary2.java
@@ -0,0 +1,8 @@
+public class unary2 {
+    public static void main(String[] args) {
+        int a= 15;
+        int b= --a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
