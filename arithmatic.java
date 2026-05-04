cdb7f43a23ab1baf568d87b92ef667d661aa3d94
shriraj kharade <shrirajkharade@gmail.com>
2026-05-04 10:53:55 +0530

    arithmatic operator

---
+++ arithmatic.java
@@ -0,0 +1,11 @@
+
+public class arithmatic {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println("addition =" + (a+b));
+        System.out.println("subtraction =" + (a-b));
+        System.out.println("multiplication =" + (a*b));
+    }
+    
+}
