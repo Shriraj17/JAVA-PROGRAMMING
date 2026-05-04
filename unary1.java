bc76585840679dfb657c750069f0bf2091156faf
shriraj kharade <shrirajkharade@gmail.com>
2026-05-04 10:59:53 +0530

    unary operators

---
+++ unary1.java
@@ -0,0 +1,11 @@
+public class unary1 {
+    public static void main(String[] args) {
+        int a=10;
+        int b= ++a;
+        int c= a++;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
