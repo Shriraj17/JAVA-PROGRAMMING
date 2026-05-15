26c86bf9052046ba444b008a80cc809faa0baf8c
shriraj kharade <shrirajkharade@gmail.com>
2026-05-15 17:03:32 +0530

    largest of two numbers

---
+++ largestno.java
@@ -0,0 +1,13 @@
+public class largestno {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 22;
+
+        if(a>=b){
+            System.out.println("a is the largest");
+        }
+        else{
+            System.out.println("b is the largest");
+        }
+    }
+}
