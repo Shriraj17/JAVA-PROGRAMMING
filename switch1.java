884cb3516115fa7f23735894428b3b3a634ddce0
shriraj kharade <shrirajkharade@gmail.com>
2026-05-16 17:34:51 +0530

    switch statement

---
+++ switch1.java
@@ -0,0 +1,22 @@
+
+
+public class switch1 {
+    public static void main(String[] args) {
+       int number = 1;
+       switch(number){
+        case 1:
+            System.out.println("ramos");
+            break;
+        case 2:
+            System.out.println("valverde");
+            break;
+        case 3:
+            System.out.println("ronaldo");
+            break;
+        default:
+            System.out.println("nobody else");
+ 
+    
+       } 
+    }
+}
