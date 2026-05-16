814a66f7dc1a31ac8502bed42ccef5a806cd98d4
shriraj kharade <shrirajkharade@gmail.com>
2026-05-16 17:55:41 +0530

    while loop

---
+++ while1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class while1 {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter<10) {
+            System.out.println("hala madrid");
+            counter++;
+            
+            
+        }
+    }
+    
+}
